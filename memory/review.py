7bbbda11d258fe976a59faa0c61d421c6397d470
Hack Chyson <chyson@aliyun.com>
2019-03-17 12:24:47 +0800

    memory: alter time format; add more config; and end mark;

--- memory/review.py
+++ memory/review.py
@@ -6,26 +6,31 @@ import json
 
 # json configuration file
 if len(sys.argv) <= 1:
-    json_config = "/home/hack/PycharmProjects/tools/memory/review.json"
+    json_config = "/Users/hack/PycharmProjects/tools/memory/review.json"
 else:
     json_config = sys.argv[1]
 
 with open(json_config, "r") as json_file:
     data = json.load(json_file)
 
-# paths to scan
-review_paths = data["paths"]
-# if the fullpath contains the elements in the following list, do not scan the content
-excludes = data["excludes"]
+review_paths = data["paths"]  # paths to scan
+excludes = data["excludes"]  # if the fullpath contains the elements in the following list, do not scan the content
 output_dir = data["output_dir"]
 file_format = data["file_format"]
 stop_line = data['stop_line']
 forgetting = data['forgetting']
+time_format = data['time_format']
+time_pattern = data['time_pattern']
+encoding = data["encoding"]
+headline_pattern = data["headline_pattern"]
+end = data['end']
+# This is for org-mode
+# the filename is the first level and the others degrade on level
+time_compile = re.compile(time_pattern)
+headline_compile = re.compile(headline_pattern)
 
-# if the modify time of a file is earlier than one month,
-# pass it to reduce the process time
-stop_line_ago = time.time() - stop_line * 24 * 60 * 60
-output_filename = output_dir + time.strftime(file_format)
+stop_line_ago = time.time() - stop_line * 24 * 60 * 60  # only scan file's modified time in stop line
+output_filename = output_dir + "/" + time.strftime(file_format)
 
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
@@ -35,38 +40,16 @@ if os.path.exists(output_filename):
     os.remove(output_filename)
 
 # forgetting law
-now = time.time()
+today_str = time.strftime(time_format, time.localtime())
+today_zero = time.mktime(time.strptime(today_str, time_format))
 oneday = 24 * 60 * 60
-half_day = 12 * 60 * 60
-periods = [(now - day * oneday, now - (day - 1) * oneday) for day in forgetting]
-
-# first_point = now - half_day
-# second_point = now - oneday
-# third_point = now - 2 * oneday
-# fourth_point = now - 4 * oneday
-# fifth_point = now - 7 * oneday
-# sixth_point = now - 15 * oneday
-#
-# first_period = first_point, now
-# second_period = second_point, now
-# third_period = third_point, third_point + oneday
-# fourth_period = fourth_point, fourth_point + oneday
-# fifth_period = fifth_point, fifth_point + oneday
-# sixth_period = sixth_point, sixth_point + oneday
-#
-# periods = first_period, second_period, third_period, fourth_period, fifth_period, sixth_period
-
-# the time format used in my emacs notes is [2018-12-16 21:03:43]
-time_pattern_format = "[# ]*\\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\\]"
-time_pattern = re.compile(time_pattern_format)
-headline_pattern_format = "^\\*+ "
-headline_pattern = re.compile(headline_pattern_format)
+periods = [(today_zero - day * oneday, today_zero - (day - 1) * oneday) for day in forgetting]
 
 
 # to check the time is in the review time or not
 def between_time(to_check_time):
     # strptime: string parse time, return structured time, a tuple
-    parsed_datetime = time.strptime(to_check_time.strip('\n').strip('#').strip(' '), '[%Y-%m-%d %H:%M:%S]')
+    parsed_datetime = time.strptime(to_check_time.strip('\n').strip(' '), time_format)
     sec = time.mktime(parsed_datetime)
 
     for i in periods:
@@ -78,11 +61,11 @@ def between_time(to_check_time):
 # extract content from file needed to review and write into a review file
 def extract(input_filename):
     content_start = False
-    input_file = open(input_filename, 'r', encoding='utf8')
-    output_file = open(output_filename, 'a', encoding='utf8')
+    input_file = open(input_filename, 'r', encoding=encoding)
+    output_file = open(output_filename, 'a', encoding=encoding)
     try:
         for line in input_file:
-            match_result = time_pattern.match(line)
+            match_result = time_compile.match(line.strip('\n'))
             if match_result is not None:
                 if between_time(match_result.string) is True:
                     content_start = True
@@ -90,9 +73,11 @@ def extract(input_filename):
                     # output_file.write(match_result.string)
                 else:
                     content_start = False
+            if end == line.strip('\n'):
+                content_start = False
             if match_result is None and content_start is True:
                 # if the line is headlines, demote one level to make the structure more nice looking
-                if headline_pattern.match(line) is not None:
+                if headline_compile.match(line) is not None:
                     output_file.write("*" + line)
                 else:
                     output_file.write(line)
@@ -113,8 +98,7 @@ for path in review_paths:
         for file in files:
             fullpath = root + '/' + file
             mtime = os.path.getmtime(fullpath)
-            # pass the files that earlier than one month
-            if mtime < stop_line_ago:
+            if mtime < stop_line_ago:  # pass the files that earlier than stop line
                 continue
             if include():
                 extract(fullpath)
--- memory/test.py
+++ memory/test.py
@@ -1,5 +1,35 @@
 import json
+import re
 
 json_config = '/Users/hack/PycharmProjects/tools/memory/review.json'
 with open(json_config, "r") as json_file:
     data = json.load(json_file)
+time_format = data["time_pattern"]
+print(time_format)
+time_format = re.compile(time_format)
+
+s = "[2019-01-01 Mon]"
+print(time_format.match(s))
+
+import datetime
+import time
+
+print(time.time())
+print(time.asctime())
+print(time.struct_time)
+print(time.localtime())
+print(time.strftime("%Y-%m-%d %a", time.localtime()))
+ptime1 = time.strptime("2019-03-17 Sun", "%Y-%m-%d %a")
+ptime2 = time.strptime("2019-03-16 Sat", "%Y-%m-%d %a")
+ptime3 = time.strptime("2019-03-15 Fri", "%Y-%m-%d %a")
+
+print(ptime1 > ptime2 > ptime3)
+print(ptime1 >= ptime1)
+
+print(ptime1)
+
+print(data.get("time_format"))
+a = 'hello'
+print(a=='hello')
+
+print(len('ooo   '.strip(' ')))
