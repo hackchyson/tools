c8d6dce63a1a242b1cca475d4e8ccdfd215da0c9
Hack Chyson <chyson@aliyun.com>
2019-09-07 23:49:18 +0800

    aes append;

--- aes/aes.py
+++ aes/aes.py
@@ -1,18 +1,21 @@
-# Author: Hack Chyson
-# [2018-12-20 11:27:27]
-
-import sys
+import os
 import base64
-# package: cryptodome
+import argparse
 from Crypto.Cipher import AES
 
+# commandline arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-k', '--key', required=True, help='the key used in encryption and decryption')
+ap.add_argument('-m', '--mode', default='enc',
+                help='mode of encrypt or decrypt. default is enc. available value: enc|dec')
+ap.add_argument('-i', '--input', help='input string you want to append to a file')
+ap.add_argument('-f', '--file', help='file you want to append to or decrypt from.')
+ap.add_argument('-e', '--encoding', default='utf8', help='encoding use in encryption and decryption. default is utf8')
+args = vars(ap.parse_args())
 
-# all the encrypt and decrypt operations is based on binary data
-# this avoid many encoding problems
-# base64 is used for human reading
-# because you cannot copy a binary content in the console or somewhere else
-# base64 can convert any character,
-# so it is used between binary and character
+if args['file'] is None:
+    args['file'] = os.path.join(os.environ['HOME'], '.aes')
+
 
 # if the text is not a multiple of 16, add some characters
 # so that it become a multiple of 16
@@ -22,35 +25,33 @@ def multiple_of_16(text):
     return str.encode(text)
 
 
-# key = '123456'
-# text = 'hello world'
-mode = sys.argv[1]
-key = sys.argv[2]
-text = sys.argv[3]
-
-# all the encryption and decryption are based on binary
-key_bin = multiple_of_16(key)
-text_bin = multiple_of_16(text)
+key_bin = multiple_of_16(args['key'])
 aes = AES.new(key_bin, AES.MODE_ECB)  # init cipher
 
 
-
-def encrypt():
-    encrypted_bin = aes.encrypt(text_bin)
-    encrypted_text = str(base64.encodebytes(encrypted_bin), encoding='utf8').rstrip('\n')  # ?
+# encrypt a line into a encrypted line
+def enc(line):
+    encrypted_bin = aes.encrypt(multiple_of_16(line))
+    encrypted_text = str(base64.encodebytes(encrypted_bin), encoding=args['encoding'])
     return encrypted_text
 
 
-def decrypt():
-    to_decrypt_bin = base64.decodebytes(bytes(text, encoding='utf8'))
+# decrypt a line into a decrypted line
+def dec(line):
+    to_decrypt_bin = base64.decodebytes(bytes(line, encoding=args['encoding']))
     decrypted_bin = aes.decrypt(to_decrypt_bin)
     decrypted_text = str(decrypted_bin.decode('utf8')).rstrip(' ')
     return decrypted_text
 
 
-if 'enc' == mode or 'encrypt' == mode:
-    print(encrypt())
-elif 'dec' == mode or 'decrypt' == mode:
-    print(decrypt())
+if args['mode'] == 'enc':
+    assert args['input'] is not None, 'the input should not be none'
+    with open(args['file'], 'a') as fh:
+        fh.write(enc(args['input']))
+elif args['mode'] == 'dec':
+    assert os.path.exists(args['file']), 'the file does not exist'
+    with open(args['file'], 'r') as fh:
+        for line in fh:
+            print(dec(line))
 else:
-    print('usage: python aes.py enc|dec <key> <text>')
+    print('the mode should be enc or dec')
